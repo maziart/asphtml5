19c202a6bca2e0d7316e272a2ef8f362a1cdc697
MaziarTaheri_cp <SND\MaziarTaheri_cp>
2011-12-31 20:34:36 +0000

    ScriptResourceHandler added for UpdatePanel problem with html5 inputs

---
+++ DotM.Html5/Html5/WebControls/RangeInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotM.Html5.WebControls
+{
+    /// <summary>
+    ///  Represents an imprecise control for setting the element’s value to a string representing a number.
+    /// </summary>
+    public class RangeInput : NumberInput
+    {
+        /// <summary>
+        /// Creates new instance of <c>RanceInput</c>
+        /// </summary>
+        public RangeInput() : base(InputType.Range) { }
+    }
+}
