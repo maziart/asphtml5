440ba4763380714a92a97b7a64a41d8cb71c3620
MaziarTaheri_cp <SND\MaziarTaheri_cp>
2012-01-05 12:07:48 +0000

    Addes Html5 Herald simulation as Sample

---
+++ DotM.Html5/Samples/ViewCode.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace Samples
+{
+    public partial class ViewCode : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var fileName = GetFileName();
+            PrintFile(fileName);
+        }
+
+        private void PrintFile(string fileName)
+        {
+            LblHeader.Text = "Showing Code of file :" + fileName;
+            try
+            {
+                var filePath = Server.MapPath(fileName);
+                codeContainer.InnerHtml = Server.HtmlEncode(File.ReadAllText(filePath));
+            }
+            catch (Exception ex)
+            {
+                LblFooter.Text = ex.Message;
+            }
+        }
+
+        private string GetFileName()
+        {
+            var p = Request.QueryString["p"];
+            if (string.IsNullOrEmpty(p))
+                p = "ViewCode.aspx";
+            return "~/" + p;
+        }
+    }
+}
